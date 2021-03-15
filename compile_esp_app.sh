b6bc2fa73ab04a4a655cb9c2d6f23e5e1f236f5b
schulztr <schulztr@uni-bremen.de>
2021-03-15 19:09:42 +0100

    fix scripts #3

--- compile_esp_app.sh
+++ compile_esp_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r ./sensor_nodes/bin
 export PATH="$PATH:$HOME/esp/xtensa-esp32-elf/bin"
 export ESP32_SDK_DIR=$HOME/esp/esp-idf
+make clean
 make BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf DHT=1
--- flash_esp_app.sh
+++ flash_esp_app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -r ./sensor_nodes/bin
 export PATH=$PATH:$HOME/esp/xtensa-esp32-elf/bin
 export ESP32_SDK_DIR=$HOME/esp/esp-idf
-make flash BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf DHT=1
+make clean flash BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf RAIN=1 PORT=/dev/ttyUSB1
