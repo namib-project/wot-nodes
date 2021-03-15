92f71c17ca252efaec08c59c2d24952bc44871ef
schulztr <schulztr@uni-bremen.de>
2021-03-15 18:13:50 +0100

    update skripts (#3)

--- compile_esp_app.sh
+++ compile_esp_app.sh
@@ -1,4 +1,5 @@
-#!/bin/bash  
+#!/bin/bash
+rm -r ./sensor_nodes/bin
 export PATH="$PATH:$HOME/esp/xtensa-esp32-elf/bin"
 export ESP32_SDK_DIR=$HOME/esp/esp-idf
-make BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf
+make BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf DHT=1
