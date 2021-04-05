77fcdca48e70c3875edef741c7f24a9ab279c36c
Jan Romann <jan.romann@uni-bremen.de>
2021-04-06 00:42:39 +0200

    Remove comment header from main.c

--- sensor_nodes/main.c
+++ sensor_nodes/main.c
@@ -1,23 +1,3 @@
-/*
- * Copyright (c) 2015-2016 Ken Bannister. All rights reserved.
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup     examples
- * @{
- *
- * @file
- * @brief       gcoap example
- *
- * @author      Ken Bannister <kb2ma@runbox.com>
- *
- * @}
- */
-
 #include <stdio.h>
 #include "msg.h"
 
