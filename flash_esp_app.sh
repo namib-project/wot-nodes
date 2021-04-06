5fd4478a659ff23e5330cb37a56488cca3d6df40
Jan Romann <jan.romann@uni-bremen.de>
2021-04-06 16:20:49 +0200

    Remove RAIN and PORT from flash script

--- flash_esp_app.sh
+++ flash_esp_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=$PATH:$HOME/esp/xtensa-esp32-elf/bin
 export ESP32_SDK_DIR=$HOME/esp/esp-idf
-make clean flash BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf RAIN=1 PORT=/dev/ttyUSB1
+make clean flash BOARD=esp32-wroom-32 ESP32_SDK_DIR=$HOME/esp/esp-idf
